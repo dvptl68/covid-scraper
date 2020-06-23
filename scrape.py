9e4ca5a87086f4605b73cfad3fc703a2df3b78eb
Dev Patel <dvptl68@gmail.com>
2020-06-23 08:41:37 -0400

    Improve structure

--- scrape.py
+++ scrape.py
@@ -4,8 +4,9 @@ from bs4 import BeautifulSoup
 def getData(url): return BeautifulSoup(requests.get(url).content, 'html.parser')
 
 url = 'https://en.wikipedia.org/wiki/Template:COVID-19_pandemic_data'
-soup = getData(url)
-totalData = soup.find(class_='sorttop')
-print('Cases: ' + totalData.contents[5].getText(strip=True))
-print('Deaths: ' + totalData.contents[7].getText(strip=True))
-print('Recoveries: ' + totalData.contents[9].getText(strip=True))
+mainTable = getData(url).find(id='thetable')
+data = {}
+
+totalData = mainTable.find(class_='sorttop')
+data.update({'worldwide': [totalData.contents[5].getText(strip=True), totalData.contents[7].getText(strip=True), totalData.contents[9].getText(strip=True)]})
+print(data)
