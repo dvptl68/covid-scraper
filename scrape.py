7448e04df74057040273216d0880b42ab9f1d3f8
Dev Patel <dvptl68@gmail.com>
2020-06-23 08:25:36 -0400

    Implement initial data extraction

--- scrape.py
+++ scrape.py
@@ -4,4 +4,8 @@ from bs4 import BeautifulSoup
 def getData(url): return BeautifulSoup(requests.get(url).content, 'html.parser')
 
 url = 'https://en.wikipedia.org/wiki/Template:COVID-19_pandemic_data'
-print(getData(url).prettify())
+soup = getData(url)
+totalData = soup.find(class_='sorttop')
+print('Cases: ' + totalData.contents[5].getText(strip=True))
+print('Deaths: ' + totalData.contents[7].getText(strip=True))
+print('Recoveries: ' + totalData.contents[9].getText(strip=True))
