0b786bb757207ba7fed030a6e7f6043557ace795
Dev Patel <dvptl68@gmail.com>
2020-06-23 07:52:35 -0400

    Implement bs4 HTML parsing

--- scrape.py
+++ scrape.py
@@ -1,6 +1,7 @@
 import requests
+from bs4 import BeautifulSoup
 
-url = ''
-response = requests.get(url)
-html = response.content
-print(html)
+def getData(url): return BeautifulSoup(requests.get(url).content, 'html.parser')
+
+url = 'https://en.wikipedia.org/wiki/Template:COVID-19_pandemic_data'
+print(getData(url).prettify())
