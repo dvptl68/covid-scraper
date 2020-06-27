f821c34fb41c182ddfae072b1d81871ad3becd2d
Dev Patel <dvptl68@gmail.com>
2020-06-27 16:06:24 -0400

    Implement responsive vertical positioning

---
+++ website/js/script.js
@@ -0,0 +1,18 @@
+// Function to set element positions
+const setPositions = () => {
+
+  // Get elements that need to be styled
+  const titleBlock = document.getElementById('title-block');
+  const infoBlock = document.getElementById('info-block');
+  const formBlock = document.getElementById('selection-block');
+
+  // Set vertical position of elements
+  infoBlock.style.top = `${titleBlock.clientHeight}px`;
+  formBlock.style.top = `${titleBlock.clientHeight + infoBlock.clientHeight + 50}px`
+};
+
+// Set initial position of elements
+setPositions();
+
+// Set position of elements on window resize
+window.onresize = setPositions;
