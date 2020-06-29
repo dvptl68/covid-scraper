7c3e548f8f8eed97da510111d0ae895d2ef61cdb
Dev Patel <dvptl68@gmail.com>
2020-06-29 08:29:31 -0400

    Implement country list generation

--- website/js/script.js
+++ website/js/script.js
@@ -17,3 +17,29 @@ setPositions();
 // Set position of elements on window resize
 window.onresize = setPositions;
 
+// Form DOM element
+const form = document.getElementById('selections')
+
+// Function that creates a country selection list
+const createCountryList = () => {
+
+  // Create label for selection list
+  const label = document.createElement('LABEL');
+  label.innerHTML = 'Country: ';
+
+  // Create selection list
+  const list = document.createElement('SELECT');
+
+  // Iterate through all countries, adding them to list
+  countries.forEach(e => {
+    const opt = document.createElement('OPTION')
+    opt.innerHTML = e;
+    list.appendChild(opt);
+  });
+
+  // Add label and list to form
+  form.appendChild(label);
+  form.appendChild(list);
+};
+
+createCountryList();
