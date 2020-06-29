09a17f0a3bbdb63e998ca98307d8fb9e65cac657
Dev Patel <dvptl68@gmail.com>
2020-06-29 10:05:00 -0400

    Improve list generation

--- website/js/script.js
+++ website/js/script.js
@@ -23,19 +23,41 @@ const countrySelect = document.getElementById('country-select')
 // Add blank element to list
 countrySelect.appendChild(document.createElement('OPTION'))
 
+// Add united states to list
+const usa = document.createElement('OPTION');
+usa.innerHTML = 'United States'
+countrySelect.appendChild(usa)
+
 // Iterate through the sorted countries, adding them to list
 countries.sort().forEach(country => {
+  if (country === 'United States') return;
   const opt = document.createElement('OPTION')
   opt.innerHTML = country;
   countrySelect.appendChild(opt);
 });
 
+//Get state selection list
+const stateSelect = document.getElementById('state-select');
+
+//Add blank element to list
+stateSelect.appendChild(document.createElement('OPTION'));
+
+// Iterate through the sorted states, adding them to list
+Object.keys(states).sort().forEach(state => {
+  const opt = document.createElement('OPTION')
+  opt.innerHTML = state;
+  stateSelect.appendChild(opt);
+});
+
+// Get state and county rows
+const stateRow = document.getElementById('state-row');
+const countyRow = document.getElementById('county-row');
 
 // Change display based on country selections made
 document.getElementById('country-select').addEventListener('change', event => {
 
   // Enable/disable email selection box based on the selected country
-  if (event.target.value == '') {
+  if (event.target.value === '') {
     document.getElementById('email').value = '';
   }
   document.getElementById('email').disabled = event.target.value == '';
