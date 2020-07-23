fe6ac2c86967a23149500315ff0dab6c587a0ca4
Dev Patel <dvptl68@gmail.com>
2020-07-23 08:12:01 -0400

    Implement all listings

--- js/index-script.js
+++ js/index-script.js
@@ -38,20 +38,20 @@ setPositions();
 window.onresize = setPositions;
 
 // Get country selection list
-const countrySelect = document.getElementById('country-select')
+const countrySelect = document.getElementById('country-select');
 
 // Add blank element to list
-countrySelect.appendChild(document.createElement('OPTION'))
+countrySelect.appendChild(document.createElement('OPTION'));
 
 // Add united states to list
 const usa = document.createElement('OPTION');
-usa.innerHTML = 'United States'
-countrySelect.appendChild(usa)
+usa.innerHTML = 'United States';
+countrySelect.appendChild(usa);
 
 // Iterate through sorted countries, adding them to list
 Object.keys(countries).sort().forEach(country => {
   if (country === 'United States' || country == 'Total') return;
-  const opt = document.createElement('OPTION')
+  const opt = document.createElement('OPTION');
   opt.innerHTML = country;
   countrySelect.appendChild(opt);
 });
@@ -67,4 +67,76 @@ Object.keys(states).sort().forEach(state => {
   const opt = document.createElement('OPTION')
   opt.innerHTML = state;
   stateSelect.appendChild(opt);
+});
+
+// Get county selection list
+const countySelect = document.getElementById('county-select');
+
+// Function that fills county selection list with correct counties
+const fillCounties = state => {
+
+  // Clear current list items
+  countySelect.innerHTML = '';
+
+  // Add blank element to list
+  countySelect.appendChild(document.createElement('OPTION'));
+
+  // Fill selection list with currect counties
+  Object.keys(states[state]).sort().forEach(county => {
+    if (county == 'Total') return;
+    const opt = document.createElement('OPTION');
+    opt.innerHTML = county;
+    countySelect.appendChild(opt);
+  });
+}
+
+// Get state and county rows and separators
+const stateRow = document.getElementById('state-row');
+const stateSep = document.getElementById('state-sep');
+const countyRow = document.getElementById('county-row');
+const countySep = document.getElementById('county-sep');
+
+// Change display based on country selections made
+countrySelect.addEventListener('change', event => {
+
+  // Country name
+  const country = event.target.value;
+
+  // Hide/show state and county selection based on the selected country
+  stateRow.style.display = (country === 'United States') ? 'flex' : 'none';
+  stateSep.style.display = (country === 'United States') ? 'flex' : 'none';
+  countyRow.style.display = 'none';
+  countySep.style.display = 'none';
+
+  // Reset selections
+  stateSelect.children[0].selected = true;
+});
+
+// Change display based on state selections made
+stateSelect.addEventListener('change', event => {
+
+  // State name
+  const state = event.target.value;
+
+  // Hide/show county selection based on the selected state
+  countyRow.style.display = (state !== '' && state !== 'Utah') ? 'flex' : 'none';
+  countySep.style.display = (state !== '' && state !== 'Utah') ? 'flex' : 'none';
+
+  // Skip rest of function if selected state is Utah or none
+  if (state === '' || state =='Utah') return;
+
+  // Set section name depending on state
+  let sectionName = 'County';
+  if (state === 'Alaska') sectionName = 'Borough'; 
+  else if (state === 'Louisiana') sectionName = 'Parish';
+  else if (state === 'Rhode Island') sectionName = 'Municipality';
+
+  // Set county selection label text
+  document.getElementById('county-label').innerHTML = `${sectionName} in ${state} (optional):`;
+
+  // Fill county selections
+  fillCounties(state);
+
+  // Reset selection
+  countySelect.children[0].selected = true;
 });
